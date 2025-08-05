b64f76c49a01f54d2a744dddc76bf49347e32491
Travis Bozic <travis.bozic@hotmail.com>
2025-08-05 22:24:43 +1000

    chap 3.2.1 - feat/refactor: add writeJSON utility for DRYer code

--- cmd/api/healthcheck.go
+++ cmd/api/healthcheck.go
@@ -1,15 +1,30 @@
 package main
 
 import (
-	"fmt"
+	"encoding/json"
 	"net/http"
 )
 
 func (app *application) healthcheckHandler(w http.ResponseWriter, r *http.Request) {
-	js := `{"status": "available", "environment": %q, "version": %q}`
-	js = fmt.Sprintf(js, app.config.env, version)
+	data := map[string]string{
+		"status":      "available",
+		"environment": app.config.env,
+		"version":     version,
+	}
+
+	// marshal using json.Marshal
+	js, err := json.Marshal(data)
+	if err != nil {
+		app.logger.Error(err.Error())
+		http.Error(w, "The server encountered a problem and could not process your request", http.StatusInternalServerError)
+		return
+	}
+
+	// Append a newline to the JSON. This is just a small nicety to make it easier to
+	// view in terminal applications.
+	js = append(js, '\n')
 
 	w.Header().Set("Content-Type", "application/json")
 
-	w.Write([]byte(js))
+	w.Write(js)
 }
