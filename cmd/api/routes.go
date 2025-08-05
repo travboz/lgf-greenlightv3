9b736d12d5946bcf58ca1f7b355fbca760508149
Travis Bozic <travis.bozic@hotmail.com>
2025-08-05 23:38:52 +1000

    chap 3.6.1 - feat: create helper error methods for easier and more consistent error responses, create panic recovery middleware and wrap router with it, and change router error handlers for not found and method not allowed when random url is used and when incorrect method is used.

--- cmd/api/routes.go
+++ cmd/api/routes.go
@@ -10,6 +10,15 @@ func (app *application) routes() http.Handler {
 	// Initialize a new httprouter router instance.
 	router := httprouter.New()
 
+	// Convert the notFoundResponse() helper to a http.Handler using the
+	// http.HandlerFunc() adapter, and then set it as the custom error handler for 404
+	// Not Found responses.
+	router.NotFound = http.HandlerFunc(app.notFoundResponse)
+
+	// Likewise, convert the methodNotAllowedResponse() helper to a http.Handler and set
+	// it as the custom error handler for 405 Method Not Allowed responses.
+	router.MethodNotAllowed = http.HandlerFunc(app.methodNotAllowedResponse)
+
 	// Register the relevant methods, URL patterns and handler functions for our
 	// endpoints using the HandlerFunc() method. Note that http.MethodGet and
 	// http.MethodPost are constants which equate to the strings "GET" and "POST"
@@ -18,7 +27,7 @@ func (app *application) routes() http.Handler {
 	router.HandlerFunc(http.MethodPost, "/v1/movies", app.createMovieHandler)
 	router.HandlerFunc(http.MethodGet, "/v1/movies/:id", app.showMovieHandler)
 
-	// Return the httprouter instance.
-	return router
+	// Return the httprouter instance, wrapped in recovery middleware
+	return app.recoverPanic(router)
 
 }
