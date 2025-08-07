a173290a932b3e9944c4ea0961fb1a20f4f9fa47
Travis Bozic <travis.bozic@hotmail.com>
2025-08-08 02:37:21 +1000

    chap 7.3.1 - feat: add show movie handler and logic

--- cmd/api/movies.go
+++ cmd/api/movies.go
@@ -1,9 +1,9 @@
 package main
 
 import (
+	"errors"
 	"fmt"
 	"net/http"
-	"time"
 
 	"github.com/travboz/greenlightv3/internal/data"
 	"github.com/travboz/greenlightv3/internal/data/validator"
@@ -78,13 +78,28 @@ func (app *application) showMovieHandler(w http.ResponseWriter, r *http.Request)
 		return
 	}
 
-	movie := data.Movie{
-		ID:        id,
-		CreatedAt: time.Now(),
-		Title:     "Casino Royale",
-		Runtime:   data.Runtime(144),
-		Genres:    []string{"action", "thriller", "spy"},
-		Version:   1,
+	// movie := data.Movie{
+	// 	ID:        id,
+	// 	CreatedAt: time.Now(),
+	// 	Title:     "Casino Royale",
+	// 	Runtime:   data.Runtime(144),
+	// 	Genres:    []string{"action", "thriller", "spy"},
+	// 	Version:   1,
+	// }
+
+	// Call the Get() method to fetch the data for a specific movie. We also need to
+	// use the errors.Is() function to check if it returns a data.ErrRecordNotFound
+	// error, in which case we send a 404 Not Found response to the client.
+	movie, err := app.models.Movies.Get(id)
+	if err != nil {
+		switch {
+		case errors.Is(err, data.ErrRecordNotFound):
+			app.notFoundResponse(w, r)
+		default:
+			app.serverErrorResponse(w, r, err)
+		}
+
+		return
 	}
 
 	err = app.writeJSON(w, http.StatusOK, envelope{"movie": movie}, nil)
