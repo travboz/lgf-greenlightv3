d8a07f356cdde84977a1e3e1d9030185a99d73a9
Travis Bozic <travis.bozic@hotmail.com>
2025-08-08 02:26:37 +1000

    chap 7.2.1 - feat: add logic to insert movie using createMovieHandler

--- internal/data/movies.go
+++ internal/data/movies.go
@@ -4,6 +4,7 @@ import (
 	"database/sql"
 	"time"
 
+	"github.com/lib/pq"
 	"github.com/travboz/greenlightv3/internal/data/validator"
 )
 
@@ -46,8 +47,25 @@ type MovieModel struct {
 	DB *sql.DB
 }
 
+// The Insert() method accepts a pointer to a movie struct, which should contain the
+// data for the new record.
 func (m MovieModel) Insert(movie *Movie) error {
-	return nil
+	// Define the SQL query for inserting a new record in the movies table and returning
+	// the system-generated data.
+	query := `
+	INSERT INTO movies (title, year, runtime, genres)
+	VALUES ($1, $2, $3, $4)
+	RETURNING id, created_at, version;`
+
+	// Create an args slice containing the values for the placeholder parameters from
+	// the movie struct. Declaring this slice immediately next to our SQL query helps to
+	// make it nice and clear *what values are being used where* in the query.
+	args := []any{movie.Title, movie.Year, movie.Runtime, pq.Array(movie.Genres)}
+
+	// Use the QueryRow() method to execute the SQL query on our connection pool,
+	// passing in the args slice as a variadic parameter and scanning the system-
+	// generated id, created_at and version values into the movie struct.
+	return m.DB.QueryRow(query, args...).Scan(&movie.ID, &movie.CreatedAt, &movie.Version)
 }
 
 func (m MovieModel) Get(id int64) (*Movie, error) {
