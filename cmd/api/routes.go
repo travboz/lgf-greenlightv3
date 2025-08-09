fe8ab9d89891501f51a99469a060b295e715098d
Travis Bozic <travis.bozic@hotmail.com>
2025-08-10 03:03:38 +1000

    chap 9.1.1 - feat: parsing query parameters within urls, add a handful of helpers to parse params, add listMoviesHandler to list all movies

--- cmd/api/routes.go
+++ cmd/api/routes.go
@@ -20,9 +20,11 @@ func (app *application) routes() http.Handler {
 	router.MethodNotAllowed = http.HandlerFunc(app.methodNotAllowedResponse)
 
 	router.HandlerFunc(http.MethodGet, "/v1/healthcheck", app.healthcheckHandler)
+
+	router.HandlerFunc(http.MethodGet, "/v1/movies", app.listMoviesHandler)
 	router.HandlerFunc(http.MethodPost, "/v1/movies", app.createMovieHandler)
 	router.HandlerFunc(http.MethodGet, "/v1/movies/:id", app.showMovieHandler)
-	router.HandlerFunc(http.MethodPatch, "/v1/movies/:id", app.updateMovieHandler) // partial updatess on a resource = PATCH HTTP method, so change from PUT to PATCH
+	router.HandlerFunc(http.MethodPatch, "/v1/movies/:id", app.updateMovieHandler) // partial updates on a resource = PATCH HTTP method, so change from PUT to PATCH
 	router.HandlerFunc(http.MethodDelete, "/v1/movies/:id", app.deleteMovieHandler)
 
 	// Return the httprouter instance, wrapped in recovery middleware
