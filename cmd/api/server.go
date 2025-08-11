a927d241a4c93ba16d982b15c21e485f7699e6a2
Travis Bozic <travis.bozic@hotmail.com>
2025-08-11 17:35:08 +1000

    chap 11.2.2 - feat: adding a goroutine to app.serve() which listens for a sigint or sigterm signal and catches it

--- cmd/api/server.go
+++ cmd/api/server.go
@@ -4,6 +4,9 @@ import (
 	"fmt"
 	"log/slog"
 	"net/http"
+	"os"
+	"os/signal"
+	"syscall"
 	"time"
 )
 
@@ -16,6 +19,29 @@ func (app *application) serve() error {
 		WriteTimeout: 10 * time.Second,
 		ErrorLog:     slog.NewLogLogger(app.logger.Handler(), slog.LevelError),
 	}
+
+	// Start a background goroutine.
+	go func() {
+		// Create a quit channel which carries os.Signal values
+		quit := make(chan os.Signal, 1)
+
+		// Use signal.Notify() to listen for incoming SIGINT and SIGTERM signals and
+		// relay them to the quit channel. Any other signals will not be caught by
+		// signal.Notify() and will retain their default behavior.
+		signal.Notify(quit, syscall.SIGINT, syscall.SIGTERM)
+
+		// Read the signal from the quit channel. This code will block until a signal is
+		// received.
+		s := <-quit
+
+		// Log a message to say that the signal has been caught. Notice that we also
+		// call the String() method on the signal to get the signal name and include it
+		// in the log entry attributes.
+		app.logger.Info("caught signal", "signal", s.String())
+
+		// Exit the application with a 0 (success) status code.
+		os.Exit(0)
+	}()
 
 	// Log a "starting server" message
 	app.logger.Info("starting server", "addr", srv.Addr, "env", app.config.env)
