991cb7da1d566d05d053b48c37e82130e2a63b12
Travis Bozic <travis.bozic@hotmail.com>
2025-08-20 18:00:09 +1000

    chap appendix - fix: comment out unused creatrAuthenticationTokenHandler to pass static check

--- cmd/api/tokens.go
+++ cmd/api/tokens.go
@@ -12,73 +12,75 @@ import (
 	"github.com/pascaldekloe/jwt"
 )
 
-func (app *application) createAuthenticationTokenHandler(w http.ResponseWriter, r *http.Request) {
-	// parse the email and password from the request body
-	var payload struct {
-		Email    string `json:"email"`
-		Password string `json:"password"`
-	}
+// Being replaced by `createAuthenticationJWTHandler` for JWT token usage.
+// So, when a user logs in, they receive a JWT token.
+// func (app *application) createAuthenticationTokenHandler(w http.ResponseWriter, r *http.Request) {
+// 	// parse the email and password from the request body
+// 	var payload struct {
+// 		Email    string `json:"email"`
+// 		Password string `json:"password"`
+// 	}
 
-	err := app.readJSON(w, r, &payload)
-	if err != nil {
-		app.badRequestResponse(w, r, err)
-		return
-	}
+// 	err := app.readJSON(w, r, &payload)
+// 	if err != nil {
+// 		app.badRequestResponse(w, r, err)
+// 		return
+// 	}
 
-	// validate email and password
-	v := validator.New()
+// 	// validate email and password
+// 	v := validator.New()
 
-	data.ValidateEmail(v, payload.Email)
-	data.ValidatePasswordPlaintext(v, payload.Password)
+// 	data.ValidateEmail(v, payload.Email)
+// 	data.ValidatePasswordPlaintext(v, payload.Password)
 
-	if !v.Valid() {
-		app.failedValidationResponse(w, r, v.Errors)
-		return
-	}
+// 	if !v.Valid() {
+// 		app.failedValidationResponse(w, r, v.Errors)
+// 		return
+// 	}
 
-	// Lookup the user record based on the email address. If no matching user was
-	// found, then we call the app.invalidCredentialsResponse() helper to send a 401
-	// Unauthorized response to the client (we will create this helper in a moment).
-	user, err := app.models.Users.GetByEmail(payload.Email)
-	if err != nil {
-		switch {
-		case errors.Is(err, data.ErrRecordNotFound):
-			app.invalidCredentialsResponse(w, r)
-		default:
-			app.serverErrorResponse(w, r, err)
-		}
-		return
-	}
+// 	// Lookup the user record based on the email address. If no matching user was
+// 	// found, then we call the app.invalidCredentialsResponse() helper to send a 401
+// 	// Unauthorized response to the client (we will create this helper in a moment).
+// 	user, err := app.models.Users.GetByEmail(payload.Email)
+// 	if err != nil {
+// 		switch {
+// 		case errors.Is(err, data.ErrRecordNotFound):
+// 			app.invalidCredentialsResponse(w, r)
+// 		default:
+// 			app.serverErrorResponse(w, r, err)
+// 		}
+// 		return
+// 	}
 
-	// Check if the provided password matches the actual password for the user.
-	match, err := user.Password.Matches(payload.Password)
-	if err != nil {
-		app.serverErrorResponse(w, r, err)
-		return
-	}
+// 	// Check if the provided password matches the actual password for the user.
+// 	match, err := user.Password.Matches(payload.Password)
+// 	if err != nil {
+// 		app.serverErrorResponse(w, r, err)
+// 		return
+// 	}
 
-	// If the passwords don't match, then we call the app.invalidCredentialsResponse()
-	// helper again and return.
-	if !match {
-		app.invalidCredentialsResponse(w, r)
-		return
-	}
+// 	// If the passwords don't match, then we call the app.invalidCredentialsResponse()
+// 	// helper again and return.
+// 	if !match {
+// 		app.invalidCredentialsResponse(w, r)
+// 		return
+// 	}
 
-	// Otherwise, if the password is correct, we generate a new token with a 24-hour
-	// expiry time and the scope 'authentication'.
-	token, err := app.models.Tokens.New(user.ID, 24*time.Hour, data.ScopeAuthentication)
-	if err != nil {
-		app.serverErrorResponse(w, r, err)
-		return
-	}
+// 	// Otherwise, if the password is correct, we generate a new token with a 24-hour
+// 	// expiry time and the scope 'authentication'.
+// 	token, err := app.models.Tokens.New(user.ID, 24*time.Hour, data.ScopeAuthentication)
+// 	if err != nil {
+// 		app.serverErrorResponse(w, r, err)
+// 		return
+// 	}
 
-	// Encode the token to JSON and send it in the response along with a 201 Created
-	// status code.
-	err = app.writeJSON(w, http.StatusCreated, envelope{"authentication_token": token}, nil)
-	if err != nil {
-		app.serverErrorResponse(w, r, err)
-	}
-}
+// 	// Encode the token to JSON and send it in the response along with a 201 Created
+// 	// status code.
+// 	err = app.writeJSON(w, http.StatusCreated, envelope{"authentication_token": token}, nil)
+// 	if err != nil {
+// 		app.serverErrorResponse(w, r, err)
+// 	}
+// }
 
 func (app *application) createPasswordResetTokenHandler(w http.ResponseWriter, r *http.Request) {
 	// Parse and validate the user's email address.
